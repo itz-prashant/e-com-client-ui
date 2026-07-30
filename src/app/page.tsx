f768acbaa5aaf4ea2c23fd4be99dc14578665ab1
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-30 21:19:09 +0530

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome to Pizza shop</h1>
+      <Button >Button</Button>
     </main>
   );
 }
