97b5a1e918bcc0bd46ca6b28e169734ffa702b5a
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-31 20:44:08 +0530

    add manrop font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "min-h-screen bg-background antialiased",
+        manrope.variable
+      )}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
