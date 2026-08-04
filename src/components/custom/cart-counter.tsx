51cb51eb01c37901c8bd80be34dfdee8ab4dfc45
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-04 21:33:08 +0530

    fix Cart Items Count

--- src/components/custom/cart-counter.tsx
+++ src/components/custom/cart-counter.tsx
@@ -1,16 +1,18 @@
 "use client";
 
+import { useAppSelector } from "@/lib/store/hooks";
 import { ShoppingBasketIcon } from "lucide-react";
 import Link from "next/link";
 
 const CartCounter = () => {
+    const cartItems = useAppSelector((state)=> state.cart.cartItem)
   return (
     <div className="relative">
       <Link href={"cart"}>
         <ShoppingBasketIcon className="hover:text-primary" />
       </Link>
       <span className="absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white">
-        3
+        {cartItems.length}
       </span>
     </div>
   );
