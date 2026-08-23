458a19f564d0e5543d62514722b140a671858e6b
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-23 21:01:26 +0530

    change stepper programmatically

--- src/app/order/[orderId]/components/order-status.tsx
+++ src/app/order/[orderId]/components/order-status.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Step, StepItem, Stepper } from "@/components/stepper";
+import { Step, StepItem, Stepper, useStepper } from "@/components/stepper";
 import {
   CheckCheck,
   FileCheck,
@@ -8,6 +8,7 @@ import {
   Package,
   PackageCheck,
 } from "lucide-react";
+import { useEffect } from "react";
 
 const steps = [
   {
@@ -29,10 +30,22 @@ const steps = [
   { label: "Delivered", icon: CheckCheck, description: "Order completed" },
 ] satisfies StepItem[];
 
+const StepperChanger = ()=>{
+    const  {setStep, nextStep} = useStepper()
+
+    useEffect(()=>{
+        setInterval(()=>{
+            nextStep()
+        },2000)
+    },[])
+
+    return <></>
+}
+
 const OrderStatus = () => {
   return (
     <Stepper
-      initialStep={3}
+      initialStep={0}
       steps={steps}
       variant="circle-alt"
       className="py-8"
@@ -42,6 +55,7 @@ const OrderStatus = () => {
           <Step key={label} label={label} icon={icon} checkIcon={icon}></Step>
         );
       })}
+      <StepperChanger />
     </Stepper>
   );
 };
